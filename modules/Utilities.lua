90c4bb65f6aed53d577df907335a16e51aefd11a
Adrian L Lange <adrianlund@gmail.com>
2014-08-19 20:48:09 +0200

    Override the bag portrait's OnClick for our functionality
    - This is only ever used on WoD and the sort feature, so we check if the OnClick script exists

--- modules/Utilities.lua
+++ modules/Utilities.lua
@@ -1,10 +1,15 @@
 
-local function OnClick(self)
-	PutItemInBag(self:GetID() + 19)
+local function OnClick(self, ...)
+	if(CursorHasItem()) then
+		PutItemInBag(self:GetID() + 19)
+	elseif(self.origOnClick) then
+		self.origOnClick(self, ...)
+	end
 end
 
 for index = 2, 5 do
 	local portrait = _G['ContainerFrame' .. index .. 'PortraitButton']
+	portrait.origOnClick = portrait:GetScript('OnClick')
+	portrait:SetScript('OnClick', OnClick)
 	portrait:HookScript('OnReceiveDrag', OnClick)
-	portrait:HookScript('OnClick', OnClick)
 end
